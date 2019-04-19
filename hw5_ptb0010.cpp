0e23548d2f6eb9c78330fa2da1d557ae33fa4058
perryBunn <eagle4ever20@gmail.com>
2019-04-19 16:23:12 -0500

    Start HW5 - Created base classes Node and LinkedList - Created initialization and newQuestion functions

--- hw5_ptb0010.cpp
+++ hw5_ptb0010.cpp
@@ -11,32 +11,36 @@ using namespace std;
 template<class T>
 class Node {
 public:
-    T data;
+    T data1;
+    T data2;
+    int weight;
 
-    struct Node *next;
-    struct Node *prev;
+    struct Node<T> *next;
+    struct Node<T> *prev;
 
-    Node<T>() {
-        &data = NULL;
-        *next = NULL;
-        *prev = NULL;
+
+    Node<T>(T data1In, T data2In, int weightIn) {
+        data1 = data1In;
+        data2 = data2In;
+        weight = weightIn;
+        next = nullptr;
+        prev = nullptr;
     }
 
-    Node<T>(T dataIn) {
-        &data = dataIn;
-        *next = NULL;
-        *prev = NULL;
+    Node<T>(T data1In, T data2In, int weightIn, Node *prevIn) {
+        data1 = data1In;
+        data2 = data2In;
+        weight = weightIn;
+        next = nullptr;
+        prev = prevIn;
     }
 
-    Node<T>(T dataIn, Node *nextIn) {
-        &data = dataIn;
-        *next = nextIn;
-        *prev = NULL;
-    }
-    Node<T>(T dataIn, Node *nextIn, Node *prevIn) {
-        &data = dataIn;
-        *next = nextIn;
-        *prev = prevIn;
+    Node<T>(T data1In, T data2In, int weightIn, Node *nextIn, Node *prevIn) {
+        data1 = data1In;
+        data2 = data2In;
+        weight = weightIn;
+        next = nextIn;
+        prev = prevIn;
     }
 
     string toString();
@@ -55,24 +59,25 @@ void Node<T>::print() {
 
 template<class T>
 class LinkedList {
-private:
+public:
     Node<T> *head, *tail;
 
-public:
-    LinkedList<T>() {
-        *head = NULL;
-        *tail = NULL;
-    };
+    LinkedList<T>() = default;;
 
-    LinkedList<T>(T headIn, T tailIn) {
-        *head = headIn;
-        *tail = tailIn;
+    LinkedList<T>(Node<T> *headIn) {
+        head = headIn;
+        tail = headIn;
+    }
+
+    LinkedList<T>(Node<T> headIn, Node<T> tailIn) {
+        head = &headIn;
+        tail = &tailIn;
     }
 
     bool isEmpty();
-    void push(T node);
-    void insert(int index, T node);
-    void append(T node);
+    void push(Node<T> node);
+    void insert(int index, Node<T> node);
+    void append(Node<T> *node);
     void remove(T data);
     int size();
     void toString();
@@ -84,14 +89,14 @@ bool LinkedList<T>::isEmpty() {
 }
 
 template<class T>
-void LinkedList<T>::push(T node){
+void LinkedList<T>::push(Node<T> node){
     head->prev = node;
     node.next = head;
     this->head = node;
 }
 
 template<class T>
-void LinkedList<T>::insert(int index, T node){
+void LinkedList<T>::insert(int index, Node<T> node){
     if (index == 0) {
         append(node);
         return;
@@ -110,9 +115,9 @@ void LinkedList<T>::insert(int index, T node){
 }
 
 template<class T>
-void LinkedList<T>::append(T node) {
-    tail->next = node;
-    node.prev = tail;
+void LinkedList<T>::append(Node<T> *node) {
+    this->tail->next = node;
+    node->prev = this->tail;
     this->tail = node;
 }
 
@@ -145,13 +150,59 @@ template<class T>
 void LinkedList<T>::toString() {
     string print;
     Node<T> *current = head;
-    while (current != NULL) {
-        print += current->data + " ";
+    while (current != nullptr) {
+        print += current->data1 + ", " + current->data2 + ", " + to_string(current->weight) + " -> ";
     }
     cout << print;
 }
 
+// Initialized the linked list for the program.
+LinkedList<string> init(LinkedList<string> list) {
+    Node<string> question1 = Node<string>("How long was the shortest war on record?", "38", 100);
+    Node<string> question2 = Node<string>("What was the Bank of America's original name? (Hint: Bank of Italy or Bank of Germany)", "Bank of Italy", 50);
+    Node<string> question3 = Node<string>("What is the best-selling video game of all time? (Hint: Minecraft or Tetris)", "Tetris", 20);
+
+    list = LinkedList<string>(&question1);
+    list.append(&question2);
+    list.append(&question3);
+    return list;
+}
+
+// Creates new questions to be added to the linked list
+void newQuestions(LinkedList<string> *list) {
+    bool playerConsent = true;
+    while (playerConsent) {
+        string question;
+        string answer;
+        int weight;
+
+        cout << "Enter a question: ";
+        cin >> question;
+        cout << "Enter an answer: ";
+        cin >> answer;
+        cout << "Enter award points: ";
+        cin >> weight;
+
+        Node<string> nodeQuest = Node<string>(question, answer, weight);
+        list->append(&nodeQuest);
+
+        cout << "Countinue? (Yes/No): ";
+        string response = "";
+        cin >> response;
+        if (response == "no") {
+            playerConsent = false;
+        }
+    }
+
+}
 
 int main() {
+    LinkedList<string> triviaList;
+    triviaList = init(triviaList);
+    cout << "*** Welcome to Perry's trivia quize game ***\n";
+    newQuestions(&triviaList);
+    cout << triviaList.tail->data1;
+    cout << triviaList.tail->data2;
+    cout << triviaList.tail->weight;
     return 0;
 }
